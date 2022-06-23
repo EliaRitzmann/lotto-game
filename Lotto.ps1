ee9dbbbce1a22a986145393ada1dff687652c3aa
EliaRitzmann <E.Ritzmann.inf20@stud.bbbaden.ch>
2022-06-23 07:38:37 +0200

    header

--- Lotto.ps1
+++ Lotto.ps1
@@ -1,24 +1,38 @@
+###########################################
+# Autor: Elia Ritzmann
+# Version: 1.0.0
+# Titel: Lotto game
+# Beschreibung: Übung macht den meister, 
+#               werde du der nächste multimilionär.
+# 
+# 
+###########################################
+Clear-Host
+
+
 $userNumbers = @()
-$computerNumbers =@()
+$computerNumbers = @()
 
 for ($i = 0; $i -lt 6; $i++) {
     
     do {
-        try{
-              #Typ prüfen
-              [int]$Eingabe= read-host "Bitte geben Sie eine ganze Zahl zwischen 1 und 42 ein.)"
-              if($Eingabe -gt 0 -and $Eingabe -le 42){
-                $InputOK=$true
+        try {
+            $InputOK = $false
+            #Typ prüfen
+            [int]$Eingabe = read-host "Bitte geben Sie eine ganze Zahl zwischen 1 und 42 ein.)"
+            if ($Eingabe -gt 0 -and $Eingabe -le 42) {
+                $InputOK = $true
                 $userNumbers += $Eingabe
-              }else{
-                  Write-Host "Bitte geben Sie eine gueltige Zahl ein."
-              }
-              }
+            }
+            else {
+                Write-Host "Bitte geben Sie eine gueltige Zahl ein."
+            }
+        }
         catch {
             Write-Host "Bitte geben Sie eine gueltige Zahl ein."
             
-            }
-            #Bereich prüfen
+        }
+        #Bereich prüfen
             
     } Until($InputOK -eq $true) 
     
@@ -36,7 +50,7 @@ for ($i = 0; $i -lt 6; $i++) {
     #iteration
     for ($j = 0; $j -lt 6; $j++) {
         #alternation
-        if($userNumbers[$i] -eq $computerNumbers[$j]){
+        if ($userNumbers[$i] -eq $computerNumbers[$j]) {
             $numberOfMatching++
         }
     }
@@ -48,7 +62,8 @@ Write-Host "Du hast" $numberOfMatching "Zahlen richtig geraten"
 
 if ($numberOfMatching -gt 3) {
     Write-Host "Du bist ein Lotto-Talent."
-}else {
+}
+else {
     Write-Host "Vielleicht hast du bei Sportwetten mehr Glueck."
 }
 
